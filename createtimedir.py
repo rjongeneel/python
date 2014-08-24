4d60a321c9492cdbaedb83a4c6b5fe4185981939
Raymond Jongeneel <halloraymond@gmail.com>
2014-08-24 23:00:07 +0200

    creating a directory with the name of the current time

---
+++ createtimedir.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+# createtimedir.py
+# created by Raymond Jongeneel
+# Creation date june 21, 2014
+#
+import datetime
+import os
+def createtimedir():
+    d = datetime.datetime.now()
+    datedir = '{:%Y-%m-%dH%H.%M.%S}'.format(d)
+    if not os.path.exists(datedir):
+        os.mkdir(datedir)
+
+if __name__ == "__main__":
+    createtimedir()
